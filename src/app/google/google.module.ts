0101bcd3c0ebfcf5943c9494858f7f7b8da1d78b
Thanaporn <tnpsp1501@gmail.com>
2022-02-17 19:46:40 +0700

    lab google

---
+++ src/app/google/google.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GoogleRoutingModule } from './google-routing.module';
+import { GoogleEvevtsListPageComponent } from './pages/google-events/google-evevts-list-page/google-evevts-list-page.component';
+import { GoogleComponent } from './pages/google/google.component';
+import { GoogleAuthorizationPageComponent } from './pages/google-authorization-page/google-authorization-page.component';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  declarations: [
+    GoogleEvevtsListPageComponent,
+    GoogleComponent,
+    GoogleAuthorizationPageComponent,
+  ],
+  imports: [CommonModule, GoogleRoutingModule, MatIconModule],
+})
+export class GoogleModule {}
